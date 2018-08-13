7c0d01a98b4496aebc09e98ac993917d72eabcce
Agus Cahyono <cahyo.mamen@gmail.com>
2018-08-13 10:08:58 +0700

    add border color and border width

--- DOTUI/CoreUI/DOTButton.swift
+++ DOTUI/CoreUI/DOTButton.swift
@@ -15,24 +15,48 @@ import UIKit
         sharedInit()
     }
     
+    
+    /// Set Corner Radius UIButton
     @IBInspectable var cornerRadius: CGFloat = 15 {
         didSet {
             refreshCorners(value: cornerRadius)
         }
     }
     
+    
+    /// Set Border Color UIButton
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    
+    /// Set Border Width UIButton
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    
+    /// Set Image for UIButton
     @IBInspectable var image: UIImage? {
         didSet {
             setImage(image!)
         }
     }
     
+    
+    /// Set Shadow Height UIButton
     @IBInspectable var shadowHeight: CGFloat = 0 {
         didSet {
             self.addShadow(shadowColor, height: shadowHeight)
         }
     }
     
+    
+    /// Set Shadow Color UIButton
     @IBInspectable var shadowColor: UIColor = UIColor.clear {
         didSet {
             self.addShadow(shadowColor, height: shadowHeight)
@@ -54,12 +78,22 @@ import UIKit
         addShadow(shadowColor, height: shadowHeight)
     }
     
+    
+    /// Set image for button (optional)
+    ///
+    /// - Parameter image: image icon button
     func setImage(_ image: UIImage) {
         self.setImage(image, for: .normal)
         self.imageView?.contentMode = .scaleAspectFit
         self.imageEdgeInsets = UIEdgeInsets(top: 0, left: -20, bottom: 0, right: 0)
     }
     
+    
+    /// Shadow Button
+    ///
+    /// - Parameters:
+    ///   - color: shadow color
+    ///   - height: height shadow
     func addShadow(_ color: UIColor, height: CGFloat = 5.0) {
         self.layer.shadowColor = color.cgColor
         self.layer.shadowOffset = CGSize(width: 0.0, height: height)
